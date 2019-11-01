30bd64fbb6cc0ceefa18a507bcdd023b9184df89
Arpan <arpandasgupta0@gmail.com>
2019-11-01 17:22:16 +0530

    trap 15 page fault gone

--- test_waitx.c
+++ test_waitx.c
@@ -1,18 +1,23 @@
 #include "types.h"
 #include "user.h"
 
-int main(int argc, char * argv[]) {
-    int f = fork();
+int main() {
+    for(int i = 0; i < 10; i++){
+        int f = fork();
 
-    if(f == 0) {
-        for(volatile int i = 0; i < 200000000; i++) {
-            i += 1;
-            i -= 1;
+        if(f == 0) {
+            for(volatile int i = 0; i < 2000000; i++) {
+                i += 1;
+                i -= 1;
+            }
+
+            exit();
+        } else {
+            int rtime, wtime;
+            waitx(&wtime, &rtime);
+
+            printf(1, "rtime = %d\nwtime = %d\n", rtime, wtime);
         }
-    } else {
-        int rtime, wtime;
-        waitx(&wtime, &rtime);
-
-        printf(1, "rtime = %d\nwtime = %d\n", rtime, wtime);
     }
+    exit();
 }
