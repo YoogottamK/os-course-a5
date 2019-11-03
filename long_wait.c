80576006ebad27ee4ea783c2a685f7b3f03057c6
yoogottamk <yoogottamk@outlook.com>
2019-11-03 12:14:46 +0530

    added a prog which waits for some time

---
+++ long_wait.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char * argv[]) {
+    int id = getpid();
+    for(int i = 0; i < 10; i++) {
+        printf(1, "pid: %d    %d\n", id, i);
+
+        for(volatile int j = 0; j < 5000000; j++) {
+            j += 1;
+            j -= 1;
+        }
+    }
+
+    exit();
+}
